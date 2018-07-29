59ee10997026e6625ac946a2b9d9f2720924c771
Benjamin Jones <bljones36@gmail.com>
2018-07-29 20:16:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -31,5 +31,5 @@ def prime?(number)
     return prime
 end
 
-puts Benchmark.measure {prime1?(1000000000)}
-puts Benchmark.measure {prime?(1000000000)}
+puts Benchmark.measure {prime1?(100000000)}
+puts Benchmark.measure {prime?(100000000)}
